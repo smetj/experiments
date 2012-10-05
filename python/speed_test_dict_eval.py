7f3349b3722e917435d40441a4acba585c9fef8c
Jelle Smet <development@smetj.net>
2012-10-05 23:34:47 +0200

    use testlap

--- python/speed_test_dict_eval.py
+++ python/speed_test_dict_eval.py
@@ -3,71 +3,73 @@
 import timeit
 import sys
 import platform
+from prettytable import PrettyTable
+from testlap import TestLap
 
-dictionary={"one":1}
+class DictionaryMethods():
+    '''
+    Verify an existing key in a dictionary.
+    '''
 
+    def __init__(self):
+        self.dictionary={"one":1}
 
-def test1():
-    if dictionary.has_key('one'):
-        return True
-    else:
-        return False
+    def test_1(self):
+        '''Check if an existing key exists in dictionary using has_key.'''
 
-def test2():
-    if dictionary.has_key('two'):
-        return True
-    else:
-        return False
+        if self.dictionary.has_key('one'):
+            return True
+        else:
+            return False
 
-def test3():
-    try:
-        dictionary["one"]
-        return True
-    except:
-        return False
+    def test_2(self):
+        '''Check if an non existing key exists in dictionary using has_key.'''
 
-def test4():
-    try:
-        dictionary["two"]
-        return True
-    except:
-        return False
+        if self.dictionary.has_key('two'):
+            return True
+        else:
+            return False
 
-def test5():
-    try:
-        dictionary["two"]
-        return True
-    except KeyError:
-        return False
+    def test_3(self):
+        '''Check if an existing key exists in dictionary using try except.'''
+        try:
+            self.dictionary["one"]
+            return True
+        except:
+            return False
 
-def test6():
-    if 'one' in dictionary:
-        return True
-    else:
-        return False
+    def test_4(self):
+        '''Check if a non existing key exists in dictionary using try except.'''
+        try:
+            self.dictionary["two"]
+            return True
+        except:
+            return False
 
-def output(name, iterations, seconds):
-    print "%s, %s iterations: %s seconds"% (name, iterations, seconds)
+    def test_5(self):
+        '''Check if an existing key exists in dictionary using try except KeyError.'''
+        try:
+            self.dictionary["two"]
+            return True
+        except KeyError:
+            return False
+
+    def test_6(self):
+        '''Check if an existing key exists in dictionary using in syntax.'''
+        if 'one' in self.dictionary:
+            return True
+        else:
+            return False
+
+    def test_7(self):
+        '''Check if a non existing key exists in dictionary using in syntax.'''
+        if 'two' in self.dictionary:
+            return True
+        else:
+            return False
+
 
 if __name__ == '__main__':
-    iterations=10000000
-    print sys.version
-    print platform.platform()
-
-    test_1 = timeit.Timer("test1()","from __main__ import test1")
-    output("has_key on existing key", iterations, test_1.timeit(number=iterations))
-
-    test_2 = timeit.Timer("test2()","from __main__ import test2")
-    output("has_key on non existing key", iterations, test_2.timeit(number=iterations))
-
-    test_3 = timeit.Timer("test3()","from __main__ import test3")
-    output("try/except on existing key", iterations, test_3.timeit(number=iterations))
-
-    test_4 = timeit.Timer("test4()","from __main__ import test4")
-    output("try/except all on non existing key", iterations, test_4.timeit(number=iterations))
-
-    test_5 = timeit.Timer("test5()","from __main__ import test5")
-    output("try/except KeyError on non existing key", iterations, test_5.timeit(number=iterations))
-    
-    test_6 = timeit.Timer("test6()","from __main__ import test6")
-    output("if key in dict", iterations, test_6.timeit(number=iterations))
+    instance=DictionaryMethods()
+    test_lap=TestLap(instance=instance, iterations=10000000)
+    test_lap.go()
