a4c17a5bdd8af835ec6802fe3e215f02d034efa6
Jelle Smet <jelle.smet@tomtom.com>
2013-08-27 17:34:26 +0200

    tests

--- python/wishbone/wishbone-0.4/test_wb_output_amqp.py
+++ python/wishbone/wishbone-0.4/test_wb_output_amqp.py
@@ -25,58 +25,40 @@
 
 from wishbone import Actor
 from wishbone.router import Default
-from wishbone.tools import Measure
 
-from wishbone.module import Graphite
 from wishbone.module import Null
-from wishbone.module import LogFormatFilter
+from wishbone.module import LogLevelFilter
 from wishbone.module import STDOUT
-from wishbone.module import Fanout
 from wishbone.module import Header
+from wishbone.module import TestEvent
+from wishbone.module import Fanout
 
 from wb_output_amqp import AMQP
+from wb_input_dictgenerator import DictGenerator
 from gevent import sleep, spawn
-
-class NumberGenerator(Actor):
-    def __init__(self, name, limit=0):
-        Actor.__init__(self, name, limit=limit)
-        spawn(self.run)
-
-    def consume(self, event):
-        pass
-
-    def run(self):
-        x=0
-        looper=0
-        while self.loop():
-            try:
-                if looper == 100:
-                    looper=0
-                    sleep()
-                self.queuepool.outbox.put({"header":{'broker_exchange':"", 'broker_key':"test", 'broker_tag':"test"},"data":str(x)})
-                x+=1
-                looper+=1
-            except:
-                break
-
 
 #Initialize router
 router = Default(interval=1, rescue=False, uuid=False)
-router.registerLogModule((LogFormatFilter, "logformatfilter", 0), "inbox", debug=False)
-router.registerMetricModule((Graphite, "graphite", 0), "inbox")
-router.register((STDOUT, "stdout", 0))
-router.register((Null, "null", 0))
-router.connect("logformatfilter.outbox", "stdout.inbox")
-router.connect("graphite.outbox", "null.inbox")
 
-#Consume events to STDOUT
-router.register((NumberGenerator, "numbergenerator", 0))
-router.register((Header, "header", 0), header={'broker_exchange':"", 'broker_key':"test", 'broker_tag':"test"})
-router.register((AMQP, "broker", 0), host="sandbox")
+#organize metricstream
+router.registerMetricModule(Null, "null")
+# router.registerMetricModule(Graphite, "graphite")
+# router.registerMetricModule(TCP, "graphite_transport", host=sandbox, port=2013)
+# router.connect("graphite.outbox", "graphite_transport.inbox")
 
+#organize logstream
+router.registerLogModule(LogLevelFilter, "loglevelfilter")
+router.register(STDOUT, "stdout_logs")
+router.connect("loglevelfilter.outbox", "stdout_logs.inbox")
 
-router.connect("numbergenerator.outbox", "header.inbox")
-router.connect("header.outbox", "broker.inbox")
+#router.register(TestEvent, "testevent", interval=1)
+router.register(DictGenerator, "testevent", max_elements=10)
+router.register(Header, "header", header={"amqp":{'broker_exchange':"test", 'broker_key':"test", 'broker_tag':"test"}})
+router.register(STDOUT, "stdout", complete=True)
+router.register(AMQP, "amqp", host="localhost")
+
+router.connect("testevent.outbox", "header.inbox")
+router.connect("header.outbox", "amqp.inbox")
 
 #start
 router.start()
